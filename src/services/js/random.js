ab300d463311f44e70a54fb653056a821b1efa65
Emma <emma.pernin1@gmail.com>
2022-04-25 17:26:52 +0200

    [Random.js]Update the functions to create questions and answers

--- src/services/js/random.js
+++ src/services/js/random.js
@@ -3,24 +3,24 @@ export function random(n) {
   return numberResponse;
 }
 
-export function quoteQuestion(arg, questions) {
+export function quoteQuestion(arg, questions, count) {
   for (let index = 0; index < questions.length; index++) {
-    let num = random(arg.count);
+    let num = random(count);
     let good = questions[index].correctAnswer;
-    questions[index].question = arg.results[num].content;
-    questions[index].answers[good] = arg.results[num].author;
+    questions[index].question = arg[num].content;
+    questions[index].answers[good] = arg[num].author;
   }
 }
 
-export function authorAnswers(arg, questions) {
+export function authorAnswers(arg, questions, count) {
   for (let index = 0; index < questions.length; index++) {
     let good = questions[index].correctAnswer;
     for (let indexA = 0; indexA < questions[index].answers.length; indexA++) {
-      let num = random(arg.count);
+      let num = random(count);
       if (indexA == good) {
         continue;
       } else {
-        questions[index].answers[indexA] = arg.results[num].name;
+        questions[index].answers[indexA] = arg[num].name;
       }
     }
   }
