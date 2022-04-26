d55661d6244956cca1b35a9d2e3d0cd94109242a
Emma <emma.pernin1@gmail.com>
2022-04-26 21:19:57 +0200

    [random.js] Add function for the answer

--- src/services/js/random.js
+++ src/services/js/random.js
@@ -3,25 +3,63 @@ export function random(n) {
   return numberResponse;
 }
 
-export function quoteQuestion(arg, questions, count) {
-  for (let index = 0; index < questions.length; index++) {
-    let num = random(count);
-    let good = questions[index].correctAnswer;
-    questions[index].question = arg[num].content;
-    questions[index].answers[good] = arg[num].author;
+function selectRandom(alreadySelected, list, f) {
+  let i = random(list.length);
+  let item = f(list[i]);
+  while (alreadySelected.findIndex(i => i == item) != -1) {
+    i = random(list.length);
+    item = f(list[i]);
+  }
+  return item;
+}
+function genAnswers(len, question, data, good, f) {
+  let ids = [];
+  for (let i = 0; i < len; i++) {
+    question.answers[i] = null;
+    ids.push(i);
+  }
+  let goodId = random(len);
+  ids.splice(goodId, 1);
+  question.answers[goodId] = good;
+  question.correctAnswer = goodId;
+  for (let i = 0; i < ids.length; i++) {
+    question.answers[ids[i]] = selectRandom(question.answers, data, f);
   }
 }
 
-export function authorAnswers(arg, questions, count) {
+export function quoteQuestion(arg1, questions, count1, arg2) {
   for (let index = 0; index < questions.length; index++) {
-    let good = questions[index].correctAnswer;
-    for (let indexA = 0; indexA < questions[index].answers.length; indexA++) {
-      let num = random(count);
-      if (indexA == good) {
-        continue;
-      } else {
-        questions[index].answers[indexA] = arg[num].name;
+    let num = random(count1);
+    let quote = arg1[num];
+    questions[index].question = quote.content;
+    genAnswers(4, questions[index], arg2, quote.author, f => f.name);
+  }
+}
+
+export function getResult(event, index, question) {
+  const id = index;
+  const currentQuestion = question;
+  if (id == currentQuestion.correctAnswer) {
+    console.log('Vrai', id, currentQuestion.correctAnswer);
+    event.currentTarget.classList.add('vrai');
+  } else {
+    console.log('Faux');
+    event.currentTarget.classList.add('faux');
+    const rep = document.getElementById('answer-box');
+    const ansLen = rep.children.length;
+    for (let i = 0; i < ansLen; i++) {
+      if (parseInt(rep.children[i].id) == currentQuestion.correctAnswer) {
+        rep.children[i].classList.add('vrai');
       }
     }
+  }
+  dejAns();
+}
+
+function dejAns() {
+  const rep = document.getElementById('answer-box');
+  const ansLen = rep.children.length;
+  for (let i = 0; i < ansLen; i++) {
+    rep.children[i].classList.add('deja-rep');
   }
 }
