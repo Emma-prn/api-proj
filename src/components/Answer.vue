5c430ff4cae34e3cf43b3a90d40fcfd10b42897f
Emma <emma.pernin1@gmail.com>
2022-04-26 21:19:22 +0200

    [Answer] Add class for the answer

--- src/components/Answer.vue
+++ src/components/Answer.vue
@@ -13,7 +13,7 @@ export default {
   },
   data () {
     return {
-      id: null
+      id: null,
     }
   },
   methods: {
@@ -24,13 +24,32 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .answer {
   background-color: #E0C9A6;
   color: black;
   width: 35%;
   margin: auto;
-  cursor: pointer;;
+  cursor: pointer;
+}
+.vrai {
+  background-color: green;
+  animation: fadeIn ease 1s;
+}
+.faux {
+  background-color: red;
+  animation: fadeIn ease 1s;
+}
+.deja-rep {
+    pointer-events: none;
+}
+@keyframes fadeIn {
+    0% {
+        opacity: 0.5;
+    }
+    100% {
+        opacity: 1;
+    }
 }
 @media (max-width:768px){
   .answer {
